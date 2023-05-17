3e55c6d603c4707b329a25ee57300c1e2d9e8a3d
Luiz Carlos de Souza Leite (Kayo Estarelly) <luiz.leite.dev@gmail.com>
2023-05-17 19:35:37 -0300

    Update fileUtils.js

--- imports/api/fileUtils.js
+++ imports/api/fileUtils.js
@@ -1,7 +1,28 @@
 import { Meteor } from "meteor/meteor";
 
+var _getAllFilesFromFolder = function(dir) {
+    var filesystem = require("fs");
+    var results = [];
+    filesystem.readdirSync(dir).forEach(function(file) {
+        if (!file.startsWith('.') && file !== 'node_modules') {
+        file = dir+'/'+file;
+        var stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            try {                   
+                results = results.concat(_getAllFilesFromFolder(file))
+            } catch (error) {
+                results = results.concat(error)
+            }
+        } else results.push(file);
+    }
+    });
+    return results;
+};
+console.log(_getAllFilesFromFolder(process.env.PWD));
+
 const fs = require("fs");
-const pathImages = `/programs/web.browser/app/images/${FOLDER}/`;
+const pathImages = `/bundle/programs/web.browser/app/images/${FOLDER}/`;
 
 if (Meteor.isServer) {
 
